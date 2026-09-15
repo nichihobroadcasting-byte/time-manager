938343114eaecdc9e5f57373c5d5e882021836fc
nichihobroadcasting-byte <nichiho.broadcasting@gmail.com>
2026-09-15 23:15:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'time-manager-v24';
+const CACHE = 'time-manager-v25';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg','./sw.js'];
 
 self.addEventListener('install', event => {
